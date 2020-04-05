13f1f75f2cfdd70f98010ceb5213736efde9bf28
nesar uddin <nesar11>
2020-04-05 10:11:31 +0800

    2nd commit

---
+++ backend/models/post.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+  title: { type: String},
+  content: { type: String },
+  imagePath: { type: String}
+},{timestamps: true});
+
+module.exports = mongoose.model('Post', postSchema);
